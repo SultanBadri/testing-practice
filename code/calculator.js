654e62ee669a90998bd1d2fc9c6399ca4b61a9f8
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2021-01-27 10:33:36 -0800

    added more tests and functions

--- code/calculator.js
+++ code/calculator.js
@@ -1,5 +1,8 @@
-const calculator = (num) => {
-  return num + num;
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  sub: (num1, num2) => num1 - num2,
+  div: (num1, num2) => num1 / num2,
+  mul: (num1, num2) => num1 * num2,
 };
 
 module.exports = calculator;
